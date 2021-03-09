bcd9b886d1c78308128816dcdc4d05f3257586a5
Witold Olszyk <witoldolszyk@gmail.com>
2021-03-09 23:11:46 +0100

    new styled component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [` h3 {
+    color: blue;
+  }`]
 })
 export class AppComponent {
-  name = 'Witek';
+
 }
