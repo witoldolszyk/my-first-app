a5be076cfedb7edf00993cb55a96f3884cf90579
Witold Olszyk <witoldolszyk@gmail.com>
2021-03-12 13:57:44 +0100

    server status-added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
 
-  constructor() { }
+  constructor() { setTimeout(() => {this.allowNewServer = true;}, 2000) }
 
   ngOnInit(): void {
   }
-
+ onCreateServer(){
+   this.serverCreationStatus = "Server was created :)";
+ }
 }
