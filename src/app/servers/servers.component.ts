3888993408863a5c9df8354e16226dd4ac6fa8e1
Witold Olszyk <witoldolszyk@gmail.com>
2021-03-12 15:40:55 +0100

    form - added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created";
+  serverName = "";
 
   constructor() { setTimeout(() => {this.allowNewServer = true;}, 2000) }
 
@@ -15,5 +16,8 @@ export class ServersComponent implements OnInit {
   }
  onCreateServer(){
    this.serverCreationStatus = "Server was created :)";
+ }
+ onUpdateServerName(event: Event) {
+ this.serverName = (<HTMLInputElement>event.target).value;
  }
 }
